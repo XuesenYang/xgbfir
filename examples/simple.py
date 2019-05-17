4def174507c1cec1dee15342e879dc382d34efa5
XuesenYang <42894881+XuesenYang@users.noreply.github.com>
2019-05-17 16:43:45 +0800

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -9,6 +9,7 @@
 from sklearn.datasets import load_iris, load_boston
 import xgboost as xgb
 import xgbfir
+import pandas as pd
 
 # loading database
 boston = load_boston()
@@ -28,6 +29,21 @@ xgb_cmodel = xgb.XGBClassifier().fit(iris['data'], iris['target'])
 
 # saving to file with proper feature names
 xgbfir.saveXgbFI(xgb_cmodel, feature_names=iris.feature_names, OutputXlsxFile='irisFI.xlsx')
+
+# If you want to read the local file
+# loading database
+path="/Users/yangxuesen/Downloads/"
+data=pd.read_csv(path+'heart.csv')
+print(data.describe())
+features=data.drop(['target'], axis=1)
+label=data['target']
+data_feature_names=data.columns.drop(['target'])
+
+# doing all the XGBoost magic
+xgb_cmodel = xgb.XGBClassifier().fit(features, label)
+
+# saving to file with proper feature names
+xgbfir.saveXgbFI(xgb_cmodel, feature_names=data_feature_names, OutputXlsxFile='heartFI.xlsx')
 
 
 # Check working directory. There will be two new files: **bostonFI.xlsx** and **irisFI.xlsx**.
